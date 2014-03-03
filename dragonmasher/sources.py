b572597bcc8f2623850b45a5d46323efac4dbbd0
Thomas Roten <thomas@roten.us>
2014-03-03 08:23:20 +0700

    Adds BaseRemoteSource, BaseRemoteArchiveSource, and SUBTLEX classes.

--- dragonmasher/sources.py
+++ dragonmasher/sources.py
@@ -1,17 +1,39 @@
 """Chinese data source classes and methods."""
 
+import contextlib
 import logging
+import os
 import pkgutil
+import shutil
+import sys
+import tempfile
+
+is_python3 = sys.version_info[0] > 2
+
+if is_python3:
+    from urllib.request import urlopen
+else:
+    from urllib2 import urlopen
+    from codecs import open
+
+from fcache.cache import FileCache
+from ticktock import TimeoutShelf
+
+from dragonmasher.unpack import unpack_archive
 
 logger = logging.getLogger(__name__)
+
+DEFAULT_ENCODING = 'utf-8'
+DEFAULT_TIMEOUT = 12096000
 
 
 class BaseSource(object):
     """Base class for Chinese data sources."""
 
-    def __init__(self, encoding='utf-8'):
-        self.files = None if not hasattr(self, 'files') else self.files
-        self.data = None if not hasattr(self, 'data') else self.data
+    def __init__(self, encoding=DEFAULT_ENCODING):
+        self.files = self.files if hasattr(self, 'files') else None
+        self.whitelist = self.whitelist if hasattr(self, 'whitelist') else None
+        self.data = {} if not hasattr(self, 'data') else self.data
         self.encoding = encoding
         self.key_prefix = self.name + '-'
 
@@ -23,8 +45,8 @@ class BaseSource(object):
 class BaseLocalSource(BaseSource):
     """Base class for local Chinese data sources."""
 
-    def __init__(self, encoding=None):
-        super(BaseLocalSource, self).__init__()
+    def __init__(self, encoding=DEFAULT_ENCODING):
+        super(BaseLocalSource, self).__init__(encoding=encoding)
         self.data = {}
 
     def read(self):
@@ -39,6 +61,120 @@ class BaseLocalSource(BaseSource):
     def _read_file(self, name, contents):
         """Processes and stores the file contents into self.data."""
         raise NotImplemented
+
+
+class BaseRemoteSource(BaseSource):
+    """Base class for remote Chinese data sources."""
+
+    def __init__(self, cache_data=True, cache_name='dragonmasher',
+                 timeout=DEFAULT_TIMEOUT, encoding=DEFAULT_ENCODING):
+        """Opens and reads cache data."""
+        super(BaseRemoteSource, self).__init__(encoding=encoding)
+        self.cache_data = cache_data
+        if self.cache_data:
+            self._init_cache(cache_name, timeout)
+
+    def _init_cache(self, cache_name, timeout):
+        """Opens a cache for the processed source data."""
+        cache = FileCache(cache_name, serialize=False)
+        self.cache = TimeoutShelf(cache, writeback=True, timeout=timeout)
+        self.data = self.cache.setdefault(self.name, {})
+
+    def _reset_cache(self):
+        """Deletes the cached data."""
+        del self.cache[self.name]
+        self.cache.sync()
+
+    @property
+    def has_data(self):
+        """Checks if the source data is already processed or not."""
+        return bool(self.data)
+
+    @property
+    def has_files(self):
+        """Checks if the source files are downloaded already or not."""
+        return bool(self.files)
+
+    def download(self, force_download=False, filename=None):
+        """Download the file and save it to a temporary directory."""
+        if self.has_data and not force_download:
+            logger.info("Source has cached data. Cancelling download.")
+            return self
+        elif self.has_files and not force_download:
+            logger.info("Source has unprocessed files. Cancelling download.")
+            return self
+        if force_download:
+            self._reset_cache()
+        url = self.download_url
+        logger.debug("Creating temporary directory for downloaded file.")
+        self.temp_dir = tempfile.mkdtemp()
+        rel_fname = os.path.basename(url) if filename is None else filename
+        abs_fname = os.path.join(self.temp_dir, rel_fname)
+        self._download(url, abs_fname)
+        self.files = (abs_fname,)
+        return self
+
+    def _download(self, url, filename):
+        logger.debug("Opening the URL: '%s'." % url)
+        with contextlib.closing(urlopen(self.download_url)) as page:
+            with open(filename, 'wb') as f:
+                logger.debug("Saving file: '%s'." % filename)
+                f.write(page.read())
+
+    def read(self):
+        """Reads and processes the data, then stores data in self.data."""
+        if self.has_data:
+            return self
+        elif not self.has_files:
+            raise OSError("Download was not successful, no files to read.")
+        for each in self.files:
+            rel_fname = os.path.basename(each)
+            if (self.whitelist is not None and
+                    rel_fname not in self.whitelist):
+                logger.debug("Ignoring file: '%s'." % rel_fname)
+                continue
+            logger.debug("Opening file for reading: '%s'." % each)
+            with open(each, 'r', encoding=self.encoding) as f:
+                contents = f.read()
+                logger.debug("Processing file: '%s'." % each)
+                self._read_file(each, contents)
+        if self.cache_data:
+            logger.debug("Writing processed data to a file-based cache.")
+            self.cache.sync()
+        self._cleanup()
+        return self
+
+    def _cleanup(self):
+        logger.debug("Cleaning up temporary files.")
+        shutil.rmtree(self.temp_dir)
+        del self.temp_dir
+        self.files = None
+
+
+class BaseRemoteArchiveSource(BaseRemoteSource):
+    """Base class for remote archive Chinese data sources."""
+
+    def download(self, force_download=False, filename=None):
+        """Download the file and save it to a temporary directory."""
+        if self.has_data and not force_download:
+            return self
+        super(BaseRemoteArchiveSource, self).download(force_download, filename)
+        self._extract()
+        return self
+
+    def _extract(self):
+        """Extract the contents of the archive file.
+
+        ReadError is raised when an archive cannot be read.
+
+        """
+        if not hasattr(self, 'files'):
+            raise OSError("Download was not successful, no files to extract.")
+        logger.debug("Unpacking archive file: '%s'." % self.files[0])
+        unpack_archive(self.files[0], self.temp_dir)
+        os.remove(self.files[0])
+        _files = os.listdir(self.temp_dir)
+        self.files = tuple([os.path.join(self.temp_dir, f) for f in _files])
 
 
 class CSVMixin(object):
@@ -65,30 +201,60 @@ class CSVMixin(object):
 class HSK(CSVMixin, BaseLocalSource):
     """A class for reading local HSK data."""
 
-    def __init__(self, index_column=0, encoding='utf-8'):
+    def __init__(self, index_column=0):
         self.files = ('data/hsk.csv',)
         self.headers = ('word', 'level')
         self.name = 'HSK'
-        super(HSK, self).__init__(index_column=index_column, encoding=encoding)
+        super(HSK, self).__init__(index_column=index_column, encoding='utf-8')
 
 
 class TOCFL(CSVMixin, BaseLocalSource):
     """A class for reading local TOCFL data."""
 
-    def __init__(self, index_column=0, encoding='utf-8'):
+    def __init__(self, index_column=0):
         self.files = ('data/tocfl.csv',)
         self.headers = ('word', 'level', 'pos', 'category')
         self.name = 'TOCFL'
         super(TOCFL, self).__init__(index_column=index_column,
-                                    encoding=encoding)
+                                    encoding='utf-8')
 
 
 class XianDaiChangYongZi(CSVMixin, BaseLocalSource):
     """A class for reading local XianDaiChangYongZi data."""
 
-    def __init__(self, index_column=0, encoding='utf-8'):
+    def __init__(self, index_column=0):
         self.files = ('data/xdcyz.csv',)
         self.headers = ('character', 'level', 'strokes')
         self.name = 'XDCYZ'
         super(XianDaiChangYongZi, self).__init__(index_column=index_column,
-                                                 encoding=encoding)
+                                                 encoding='utf-8')
+
+
+class SUBTLEX(BaseRemoteArchiveSource):
+    """A class for downloading and reading remote SUBTLEX data."""
+
+    download_url = ('http://expsy.ugent.be/subtlex-ch/'
+                    'SUBTLEX_CH_131210_CE.utf8.zip')
+    name = 'SUBTLEX'
+    whitelist = (
+        'SUBTLEX_CH_131210_CE.utf8',
+    )
+    headers = (
+        'word', 'length', 'pinyin', 'pinyin.input', 'wcount', 'w.million',
+        'log10w', 'w-cd', 'w-cd%', 'log10cd', 'dominant.pos',
+        'dominant.pos.freq', 'all.pos', 'all.pos.freq', 'english'
+    )
+
+    def _read_file(self, filename, contents):
+        """Processes and stores the file contents into self.data."""
+        lines = contents.splitlines()
+        for line in lines:
+            if line.startswith('Word'):
+                continue  # Skip the header.
+            row = line.split('\t')
+            key = row.pop(0)
+            row = row[:-1]
+            headers = self.headers[1:-1]
+            value = dict(zip([self.key_prefix + h for h in headers], row))
+            self.data.setdefault(key, {})
+            self.data[key].update(value)
--- dragonmasher/tests/test-sources.py
+++ dragonmasher/tests/test-sources.py
@@ -2,7 +2,10 @@
 """Unit tests for the dragonmasher.sources module."""
 
 from __future__ import unicode_literals
+import os
 import unittest
+
+from ticktock import TimeoutShelf
 
 from dragonmasher import sources
 
@@ -37,3 +40,155 @@ class LocalSourceTestCase(unittest.TestCase):
         self.assertEqual(3500, len(xdcyz.data))
         self.assertEqual('1', xdcyz.data['爱']['XDCYZ-level'])
         self.assertEqual('10', xdcyz.data['爱']['XDCYZ-strokes'])
+
+
+class BaseRemoteSourceTestCase(unittest.TestCase):
+    """Tests for the BaseRemoteSource class."""
+
+    def __init__(self, *args, **kwargs):
+        """Finds data file."""
+        data_dir = os.path.join(os.path.dirname(__file__), 'data')
+        self.data_file = os.path.join(data_dir, 'remote_source_test.txt')
+        super(BaseRemoteSourceTestCase, self).__init__(*args, **kwargs)
+
+    def setUp(self):
+        """Creates a RemoteSource instance."""
+        data_file = self.data_file
+
+        class RemoteSource(sources.BaseRemoteSource):
+            name = 'RemoteSource'
+            download_url = 'http://foo.com/remote_source_test.txt'
+
+            def _download(self, url, filename):
+                with open(data_file) as f:
+                    data = f.read()
+                with open(filename, 'w') as f:
+                    f.write(data)
+
+            def _read_file(self, f, contents):
+                self.data['test'] = contents
+
+        self.source = RemoteSource(cache_data=False)
+
+    def tearDown(self):
+        """Deletes cache files and temporary files."""
+        if self.source.cache_data:
+            self.source.cache.dict.delete()
+        if hasattr(self.source, 'temp_dir'):
+            self.source._cleanup()
+
+    def test_remote_source_init(self):
+        """Tests that BaseRemoteSource.__init__ works correctly."""
+        self.assertTrue(isinstance(self.source.data, dict))
+        self.assertFalse(self.source.cache_data)
+        self.assertFalse(hasattr(self.source, 'cache'))
+        self.source.cache_data = True
+        self.source._init_cache('dragonmasher-tests', 10)
+        self.assertTrue(isinstance(self.source.data, dict))
+        self.assertTrue(self.source.cache_data)
+        self.assertTrue(isinstance(self.source.cache, TimeoutShelf))
+
+    def test_remote_source_download(self):
+        """Tests that BaseRemoteSource.download works correctly."""
+        self.source.download()
+        self.assertTrue(hasattr(self.source, 'temp_dir'))
+        self.assertEqual(1, len(self.source.files))
+        self.assertTrue(os.path.exists(self.source.files[0]))
+
+    def test_remote_source_read_(self):
+        """Tests that BaseRemoteSource.read works correctly."""
+        self.assertRaises(OSError, self.source.read)
+        self.source.download()
+        self.source.read()
+        self.assertTrue(not hasattr(self.source, 'temp_dir'))
+        self.assertEqual(None, self.source.files)
+        self.assertEqual('Hello world!\n', self.source.data['test'])
+
+    def test_remote_source_cache(self):
+        """Tests that BaseRemoteSource caching features work correctly."""
+        self.source.cache_data = True
+        self.source._init_cache('dragonmasher-tests', 10)
+        self.source.download()
+        files = self.source.files
+        temp_dir = self.source.temp_dir
+        self.source.download()
+        self.assertEqual(files, self.source.files)
+        self.assertEqual(temp_dir, self.source.temp_dir)
+        self.source.read()
+        self.assertEqual('Hello world!\n', self.source.data['test'])
+        del self.source
+        self.setUp()
+        self.source.cache_data = True
+        self.source._init_cache('dragonmasher-tests', 10)
+        self.source.download()
+        self.assertEqual(None, self.source.files)
+        self.assertEqual('Hello world!\n', self.source.data['test'])
+
+
+class BaseRemoteArchiveSourceTestCase(unittest.TestCase):
+    """Tests for the BaseRemoteArchiveSource class."""
+
+    def __init__(self, *args, **kwargs):
+        """Finds data file."""
+        data_dir = os.path.join(os.path.dirname(__file__), 'data')
+        self.data_file = os.path.join(data_dir, 'remote_archive_test.zip')
+        self.filename = 'remote_source_test.txt'
+        super(BaseRemoteArchiveSourceTestCase, self).__init__(*args, **kwargs)
+
+    def setUp(self):
+        """Creates a RemoteArchiveSource instance."""
+        data_file = self.data_file
+
+        class RemoteArchiveSource(sources.BaseRemoteArchiveSource):
+            name = 'RemoteArchiveSource'
+            download_url = 'http://foo.com/remote_archive_test.zip'
+
+            def _download(self, url, filename):
+                with open(data_file, 'rb') as f:
+                    data = f.read()
+                with open(filename, 'wb') as f:
+                    f.write(data)
+
+        self.source = RemoteArchiveSource(cache_data=False)
+
+    def tearDown(self):
+        """Deletes cache files and temporary files."""
+        if self.source.cache_data:
+            self.source.cache.dict.delete()
+        if hasattr(self.source, 'temp_dir'):
+            self.source._cleanup()
+
+    def test_remote_archive_source_download(self):
+        """Tests that BaseRemoteArchiveSource.download works correctly."""
+        self.source.download()
+        self.assertTrue(hasattr(self.source, 'temp_dir'))
+        self.assertEqual(1, len(self.source.files))
+        self.assertTrue(os.path.exists(self.source.files[0]))
+        self.assertTrue(self.filename in self.source.files[0])
+
+
+class SUBTLEXTestCase(unittest.TestCase):
+    """Tests for the SUBTLEX data source class."""
+
+    def __init__(self, *args, **kwargs):
+        """Sets the data_dir attribute."""
+        self.data_dir = os.path.join(os.path.dirname(__file__), 'data')
+        self.data_file = os.path.join(self.data_dir, 'subtlex_words_test.txt')
+        super(SUBTLEXTestCase, self).__init__(*args, **kwargs)
+
+    def setUp(self):
+        """Reads data file."""
+        self.subtlex = sources.SUBTLEX(cache_data=False)
+        self.subtlex.files = (self.data_file,)
+        self.subtlex.whitelist = ('subtlex_words_test.txt',)
+
+        def _cleanup():
+            pass
+
+        self.subtlex._cleanup = _cleanup
+
+    def test_subtlex_words_read(self):
+        """Tests that SUBTLEXWords processes the data correctly."""
+        self.subtlex.read()
+        self.assertEqual(4, len(self.subtlex.data))
+        self.assertEqual('1', self.subtlex.data['的']['SUBTLEX-length'])
