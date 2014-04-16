1838c251a6db33f9dce67e3657be52397b8c08ba
Thomas Roten <thomas@roten.us>
2014-04-16 22:36:32 +0800

    Reformats setup.py.

--- setup.py
+++ setup.py
@@ -1,28 +1,60 @@
-from setuptools import setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-with open("README.rst") as f:
-    long_description = f.read()
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import dragonmasher
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+def open_file(filename):
+    """Open and read the file *filename*."""
+    with open(filename) as f:
+        return f.read()
+
+readme = open_file('README.rst')
+history = open_file('HISTORY.rst').replace('.. :changelog:', '')
 
 setup(
     name='Dragon Masher',
-    version='0.1dev',
+    version=dragonmasher.__version__,
+    description='Dragon Masher provides access to Chinese word/character data',
+    long_description=readme + '\n\n' + history,
     author='Thomas Roten',
     author_email='thomas@roten.us',
     url='https://github.com/tsroten/dragonmasher',
-    description='',
-    long_description=long_description,
+    packages=['dragonmasher'],
+    package_dir={'dragonmasher': 'dragonmasher'},
+    include_package_data=True,
+    package_data={'dragonmasher': ['data/*.csv']},
+    install_requires=[
+        'zhon>=1.0',
+        'ticktock>=0.1.2',
+        'fcache>=0.4.4',
+        'dragonmapper'
+    ],
+    license='BSD',
+    keywords=['dragonmasher', 'chinese', 'data', 'hsk', 'cedict', 'cc-cedict',
+              'tocfl', 'unihan', 'junda'],
     classifiers=[
-        'Programming Language :: Python',
-        'License :: OSI Approved :: MIT License',
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
     ],
-    keywords=[],
-    packages=['dragonmasher'],
-    package_data={'dragonmasher': ['data/*.csv']},
     test_suite='dragonmasher.tests',
-    install_requires=['zhon>=1.0', 'ticktock>=0.1.2', 'fcache>=0.4.4',
-                      'dragonmapper'],
 )
